456351855fe2628b9328fa573d1ec6de3c257c76
Maciec <maciekkuc1@gmail.com>
2019-06-11 18:32:29 +0200

    logic
    added the program logic and event listener

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ const button = document.querySelector('.box__button');
 const numberContainer = document.querySelector('.box__numbers');
 const input = document.querySelector('.box__input');
 
-const calculateFibonacci = (n, interval) => {
+const calculateFibonacci = (n) => {
 	let fibonacciSequence = [];
+	console.log(n);
+	console.log(typeof n);
+	if( n === undefined){
+		alert('Please enter a number.');
+	}
+	else if(n === 0){
+		fibonacciSequence.push(0);
+	}else if(n === 1){
+		fibonacciSequence.push(0,1);
+	}else{
+		fibonacciSequence.push(0,1);
+		for(let i = 2; i <=n; i++){
+			fibonacciSequence.push(fibonacciSequence[i-1] + fibonacciSequence[i-2]);
+		}
+		console.log(fibonacciSequence);
+	}
+	return fibonacciSequence;
 };
+
+const showNumbers = (inputArr, delay) => {
+	inputArr.map((item) =>{
+		var number = document.createElement('p');
+		number.innerHTML = item;
+		numberContainer.appendChild(number);
+	})
+
+};
+
+button.addEventListener('click', (e) => {
+	numberContainer.innerHTML = '';
+	let sequence = calculateFibonacci(input.value,5);
+	showNumbers(sequence,5);
+});
