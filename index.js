5f2bc56d3b7d27c38ca0e404452ffe5b28062588
Maciec <maciekkuc1@gmail.com>
2019-06-11 09:15:38 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.box__button');
+const numberContainer = document.querySelector('.box__numbers');
+const input = document.querySelector('.box__input');
+
+const calculateFibonacci = (n, interval) => {
+	let fibonacciSequence = [];
+};
